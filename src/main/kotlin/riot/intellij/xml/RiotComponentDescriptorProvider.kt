5d767d479476e303a5534081dd89f444671344a8
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-07-19 13:12:32 +0300

    riot tag support

---
+++ src/main/kotlin/riot/intellij/xml/RiotComponentDescriptorProvider.kt
@@ -0,0 +1,22 @@
+package riot.intellij.xml
+
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.html.HtmlDocumentImpl
+import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider
+import com.intellij.psi.xml.XmlTag
+import com.intellij.xml.XmlElementDescriptor
+import riot.intellij.lang.RiotLanguage
+
+class RiotComponentDescriptorProvider : XmlElementDescriptorProvider {
+    override fun getDescriptor(tag: XmlTag?): XmlElementDescriptor? {
+        if (tag?.containingFile?.language == RiotLanguage.INSTANCE) {
+            val parent = tag.parent
+            //only top tags
+            if (parent is PsiFile || parent is HtmlDocumentImpl) {
+                return RiotComponentXmlDescriptor(tag)
+            }
+        }
+
+        return null
+    }
+}
---
+++ src/main/kotlin/riot/intellij/xml/RiotComponentXmlDescriptor.kt
@@ -0,0 +1,35 @@
+package riot.intellij.xml
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlAttribute
+import com.intellij.psi.xml.XmlTag
+import com.intellij.xml.XmlAttributeDescriptor
+import com.intellij.xml.XmlElementDescriptor
+import com.intellij.xml.XmlElementDescriptor.CONTENT_TYPE_UNKNOWN
+import com.intellij.xml.XmlElementsGroup
+import com.intellij.xml.XmlNSDescriptor
+
+class RiotComponentXmlDescriptor(private val tag: XmlTag) : XmlElementDescriptor {
+
+    private val myName = tag.name
+
+    override fun getDefaultValue(): String? = null
+
+    override fun getName(p0: PsiElement?): String = name
+
+    override fun getName(): String = myName
+
+    override fun getElementsDescriptors(p0: XmlTag?): Array<XmlElementDescriptor> = emptyArray()
+    override fun init(p0: PsiElement?) {}
+    override fun getContentType(): Int = CONTENT_TYPE_UNKNOWN
+    override fun getTopGroup(): XmlElementsGroup? = null
+    override fun getDefaultName(): String = name
+    override fun getNSDescriptor(): XmlNSDescriptor? = null
+    override fun getQualifiedName(): String = name
+    override fun getElementDescriptor(p0: XmlTag?, p1: XmlTag?): XmlElementDescriptor? = null
+    override fun getDeclaration(): PsiElement? = tag
+    override fun getAttributeDescriptor(p0: String?, p1: XmlTag?): XmlAttributeDescriptor? = null
+    override fun getAttributeDescriptor(p0: XmlAttribute?): XmlAttributeDescriptor? = null
+    override fun getAttributesDescriptors(p0: XmlTag?): Array<XmlAttributeDescriptor> = emptyArray()
+
+}
