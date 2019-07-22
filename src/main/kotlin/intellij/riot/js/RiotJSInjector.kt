541992055f1df2d4fd6bde670e2fae3f38e0b1a7
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-07-22 20:07:08 +0300

    riot do not use composite elements for injections

--- src/main/kotlin/intellij/riot/js/RiotJSInjector.kt
+++ src/main/kotlin/intellij/riot/js/RiotJSInjector.kt
@@ -3,9 +3,10 @@ package intellij.riot.js
 import com.intellij.lang.injection.MultiHostInjector
 import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.lang.javascript.JSInjectionBracesUtil
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.psi.xml.XmlAttribute
-import com.intellij.psi.xml.XmlText
+import com.intellij.psi.PsiLanguageInjectionHost
+import com.intellij.psi.xml.*
 import com.intellij.util.containers.ContainerUtil
 import intellij.riot.lang.IRiotFileType
 
@@ -17,12 +18,18 @@ class RiotJSInjector : MultiHostInjector {
             JSInjectionBracesUtil
                     .injectInXmlTextByDelimiters(registrar, context, RiotJSLanguage.INSTANCE, "{", "}")
         }
-        if (context is XmlAttribute) {
-            val valueElement = context.valueElement ?: return
-            JSInjectionBracesUtil
-                    .injectInXmlTextByDelimiters(registrar, valueElement, RiotJSLanguage.INSTANCE, "{", "}")
+        if (context is XmlAttributeValue) {
+            val prevSibling = context.prevSibling
+            if (prevSibling == XmlTokenType.XML_ATTRIBUTE_VALUE_START_DELIMITER && prevSibling.text == "{") {
+                registrar.startInjecting(RiotJSLanguage.INSTANCE, null).addPlace(null as String?, null as String?, 
+                        context as PsiLanguageInjectionHost, 
+                        TextRange(0, (context as PsiElement).textLength)).doneInjecting()
+            } else {
+                JSInjectionBracesUtil
+                        .injectInXmlTextByDelimiters(registrar, context, RiotJSLanguage.INSTANCE, "{", "}")
+            }
         }
     }
 
-    override fun elementsToInjectIn(): List<Class<out PsiElement>> = ContainerUtil.immutableList(XmlText::class.java, XmlAttribute::class.java)
+    override fun elementsToInjectIn(): List<Class<out PsiElement>> = ContainerUtil.immutableList(XmlText::class.java, XmlAttributeValue::class.java)
 }
