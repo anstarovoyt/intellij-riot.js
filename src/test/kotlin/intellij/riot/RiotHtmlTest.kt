5ae73c089fde64b88369404b112d338bd371c886
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-08-09 11:49:26 +0300

    riot add tests

--- src/test/kotlin/intellij/riot/RiotHtmlTest.kt
+++ src/test/kotlin/intellij/riot/RiotHtmlTest.kt
@@ -1,12 +1,20 @@
 package intellij.riot
 
+import com.intellij.testFramework.UsefulTestCase
 import junit.framework.TestCase
 
 class RiotHtmlTest : RiotTestBase() {
     fun testCssInTags() {
         myFixture.configureByText("my.css", ".myClass {}")
-        myFixture.configureByText("test.riot", "<div class='myCla<caret>ss'></div>")
+        myFixture.configureByText("test.riot", "<Test><div class='myCla<caret>ss'></div></Test>")
         val ref = myFixture.getReferenceAtCaretPosition()
         TestCase.assertNotNull(ref?.resolve())
+    }
+    
+    fun testTagSimpleAttributes() {
+        myFixture.configureByText("test.riot", "<Test><div <caret>></div></Test>")
+        val results = myFixture.completeBasic()
+        val strings = results.map { it.lookupString }.toSet()
+        UsefulTestCase.assertContainsElements(strings, "class")
     }
 }
