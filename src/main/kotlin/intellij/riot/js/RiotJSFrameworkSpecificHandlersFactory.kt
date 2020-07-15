6fa4bda6fd4f6b42e3d26e1cd7e8491acacf68c2
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2020-07-15 11:04:48 +0300

    update, gradle, kotlin, fix compatibility with 202

--- src/main/kotlin/intellij/riot/js/RiotJSFrameworkSpecificHandlersFactory.kt
+++ src/main/kotlin/intellij/riot/js/RiotJSFrameworkSpecificHandlersFactory.kt
@@ -1,14 +1,15 @@
 package intellij.riot.js
 
 import com.intellij.lang.ecmascript6.psi.ES6ExportDefaultAssignment
-import com.intellij.lang.javascript.frameworks.JSFrameworkSpecificHandlersFactory
+import com.intellij.lang.javascript.frameworks.JSFrameworkSpecificHandler
 import com.intellij.lang.javascript.psi.*
 import com.intellij.lang.javascript.psi.types.*
 import com.intellij.psi.PsiElement
 import intellij.riot.lang.RiotHtmlLanguage
 
-class RiotJSFrameworkSpecificHandlersFactory : JSFrameworkSpecificHandlersFactory {
-    override fun findExpectedType(parent: JSExpression, expectedTypeKind: JSExpectedTypeKind): JSType? {
+class RiotJSFrameworkSpecificHandlersFactory : JSFrameworkSpecificHandler {
+
+    override fun findExpectedType(parent: PsiElement, expectedTypeKind: JSExpectedTypeKind): JSType? {
         val containingFile = parent.containingFile
         if (containingFile.language !is RiotHtmlLanguage ||
                 parent !is JSObjectLiteralExpression ||
@@ -24,10 +25,10 @@ class RiotJSFrameworkSpecificHandlersFactory : JSFrameworkSpecificHandlersFactor
         val simpleSource = JSTypeSourceFactory.createTypeSource(parent, true)
         val stringType = JSNamedTypeFactory.createType(JSCommonTypeNames.STRING_TYPE_NAME, simpleSource, JSContext.INSTANCE)
         val members = mutableListOf<JSRecordType.TypeMember>()
-        members.add(JSRecordTypeImpl.PropertySignatureImpl("components", anyType, true, parent))
-        members.add(JSRecordTypeImpl.PropertySignatureImpl("state", anyType, true, parent))
-        members.add(JSRecordTypeImpl.PropertySignatureImpl("props", anyType, true, parent))
-        members.add(JSRecordTypeImpl.PropertySignatureImpl("name", stringType, true, parent))
+        members.add(JSRecordTypeImpl.PropertySignatureImpl("components", anyType, true, false, parent))
+        members.add(JSRecordTypeImpl.PropertySignatureImpl("state", anyType, true, false, parent))
+        members.add(JSRecordTypeImpl.PropertySignatureImpl("props", anyType, true, false, parent))
+        members.add(JSRecordTypeImpl.PropertySignatureImpl("name", stringType, true, false, parent))
         members.add(createSimpleTwoArgumentFunction("shouldUpdate", simpleSource, anyType))
         members.add(createSimpleTwoArgumentFunction("onBeforeMount", simpleSource, anyType))
         members.add(createSimpleTwoArgumentFunction("onMounted", simpleSource, anyType))
@@ -44,6 +45,6 @@ class RiotJSFrameworkSpecificHandlersFactory : JSFrameworkSpecificHandlersFactor
         val props = JSParameterTypeDecoratorImpl(anyType, true, false, false)
         val state = JSParameterTypeDecoratorImpl(anyType, true, false, false)
         val jsFunctionType = JSFunctionTypeImpl(simpleSource, listOf(props, state), null)
-        return JSRecordTypeImpl.PropertySignatureImpl(name, jsFunctionType, true)
+        return JSRecordTypeImpl.PropertySignatureImpl(name, jsFunctionType, true, false)
     }
 }
