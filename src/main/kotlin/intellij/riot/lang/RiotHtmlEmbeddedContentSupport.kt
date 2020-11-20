1f3d3b91514bca5280c43864384fde2df543ac85
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2020-11-20 18:35:56 +0300

    riot minor fixes:
    1. Restore highlighting
    2. Restore ts / tsx support

--- src/main/kotlin/intellij/riot/lang/RiotHtmlEmbeddedContentSupport.kt
+++ src/main/kotlin/intellij/riot/lang/RiotHtmlEmbeddedContentSupport.kt
@@ -7,16 +7,23 @@ import com.intellij.lang.Language
 import com.intellij.lang.javascript.JavaScriptSupportLoader
 import com.intellij.lexer.BaseHtmlLexer
 import com.intellij.lexer.HtmlScriptStyleEmbeddedContentProvider
+import com.intellij.xml.util.HtmlUtil
 
 class RiotHtmlEmbeddedContentSupport : HtmlEmbeddedContentSupport {
 
-    override fun isEnabled(lexer: BaseHtmlLexer): Boolean = lexer is RiotHtmlLexer
+    override fun isEnabled(lexer: BaseHtmlLexer): Boolean {
+        return  lexer is RiotHtmlLexer || lexer is RiotHtmlHighlightingLexer
+    }
 
     override fun createEmbeddedContentProviders(lexer: BaseHtmlLexer): List<HtmlEmbeddedContentProvider> {
         return listOf(RiotHtmlScriptStyleEmbeddedContentProvider(lexer))
     }
 
     class RiotHtmlScriptStyleEmbeddedContentProvider(lexer: BaseHtmlLexer) : HtmlScriptStyleEmbeddedContentProvider(lexer) {
+
+        override fun isInterestedInAttribute(attributeName: CharSequence): Boolean {
+            return super.isInterestedInAttribute(attributeName) || namesEqual(attributeName, "lang")
+        }
 
         override fun styleLanguage(styleLang: String?): Language? =
                 (if (styleLang != null && !styleLang.startsWith("text/")) {
